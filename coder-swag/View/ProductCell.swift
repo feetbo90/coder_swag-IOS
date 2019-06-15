80fc868620f7f08256a6fb5e96ffa1cd6eb78a87
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-06-15 15:09:00 +0700

    product cell

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Muhammad Iqbal Pradipta on 15/06/19.
+//  Copyright © 2019 Muhammad Iqbal Pradipta. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
