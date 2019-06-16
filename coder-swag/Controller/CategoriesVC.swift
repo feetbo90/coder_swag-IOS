1051cfa494247920a669515d101af6e1cbc6f7bc
Muhammad Iqbal Pradipta <feetbonike90@gmail.com>
2019-06-16 11:40:58 +0700

    product display

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -20,10 +20,12 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         categoryTable.delegate = self
     }
 
+    // menghitung jumlah row
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return DataService.instance.getCategories().count
     }
     
+    // menginput nilai per row utk menjadi data
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
             let category = DataService.instance.getCategories()[indexPath.row]
@@ -31,6 +33,21 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
             return cell
         } else {
             return CategoryCell()
+        }
+    }
+    
+    // pada saat user click
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductsVC {
+            productsVC.initProducts(category: sender as! Category)
+            let barBtn = UIBarButtonItem()
+            barBtn.title = ""
+            navigationItem.backBarButtonItem = barBtn
         }
     }
 
